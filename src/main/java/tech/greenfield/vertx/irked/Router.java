675021b344fbd28867c82b2d9c78d139ef6df98f
Oded Arbel <oded@geek.co.il>
2017-07-10 21:22:25 +0300

    add support for routing to methods

--- src/main/java/tech/greenfield/vertx/irked/Router.java
+++ src/main/java/tech/greenfield/vertx/irked/Router.java
@@ -3,17 +3,22 @@ package tech.greenfield.vertx.irked;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Objects;
 import java.util.function.Function;
 
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.RoutingContext;
 import tech.greenfield.vertx.irked.annotations.*;
 
 public class Router {
+	
+	static Logger log = LoggerFactory.getLogger(Router.class);
 	
 	public class RequestWrapper implements Function<RoutingContext, Request>, Handler<RoutingContext> {
 
@@ -64,65 +69,46 @@ public class Router {
 		if (prefix.endsWith("/"))
 			prefix = prefix.substring(0, prefix.length() - 1);
 		
-		for (Field f : api.getRoutingFields()) {
-			tryConfigureRoute(api, router::route, prefix, f, Endpoint.class, requestWrapper);
-			tryConfigureRoute(api, router::post, prefix, f, Post.class, requestWrapper);
-			tryConfigureRoute(api, router::get, prefix, f, Get.class, requestWrapper);
-			tryConfigureRoute(api, router::put, prefix, f, Put.class, requestWrapper);
-			tryConfigureRoute(api, router::delete, prefix, f, Delete.class, requestWrapper);
-		};
+		for (RouteConfiguration f : api.getRoutes()) {
+			tryConfigureRoute(router::route, prefix, f, Endpoint.class, requestWrapper);
+			tryConfigureRoute(router::post, prefix, f, Post.class, requestWrapper);
+			tryConfigureRoute(router::get, prefix, f, Get.class, requestWrapper);
+			tryConfigureRoute(router::put, prefix, f, Put.class, requestWrapper);
+			tryConfigureRoute(router::delete, prefix, f, Delete.class, requestWrapper);
+		}
 	}
 
-	private <T extends Annotation> boolean tryConfigureRoute(Controller api, RoutingMethod method, 
-			String prefix, Field field, Class<T> anot, RequestWrapper requestWrapper) {
-		String path = uriFromRoutingAnnotation(field, anot);
+	private <T extends Annotation> boolean tryConfigureRoute(RoutingMethod method, 
+			String prefix, RouteConfiguration conf, Class<T> anot, RequestWrapper requestWrapper) {
+		String path = conf.uriForAnnotation(anot);
 		if (Objects.isNull(path))
 			return false;
 		path = prefix + path;
 		if (path.length() > 1 && path.endsWith("/"))
 			path = path.substring(0, path.length() - 1); // normalize extra paths
 		
-		if (Controller.class.isAssignableFrom(field.getType())) {
-			Controller ctr = Objects.requireNonNull(api.getController(field), "Sub-Controller for " + field + " is not set!");
+		if (conf.isController()) {
+			Controller ctr = Objects.requireNonNull(conf.getController(), "Sub-Controller for " + conf + " is not set!");
 			configure(ctr, path, new RequestWrapper(ctr, requestWrapper));
 			return true;
 		}
 		
 		try {
 			Route route = method.setRoute(path);
-			Handler<RoutingContext> handler = new RequestWrapper(Objects.requireNonNull(api.getHandler(field)), requestWrapper);
-			if (isFailHandler(field))
+			Handler<RoutingContext> handler = new RequestWrapper(Objects.requireNonNull(conf.getHandler()), requestWrapper);
+			if (conf.isFailHandler())
 				route.failureHandler(handler);
-			if (isBlocking(field))
+			if (conf.isBlocking())
 				route.blockingHandler(handler);
 			else
 				route.handler(handler);
 			return true;
 		} catch (NullPointerException e) {
 			// ignore NPEs created by api.getHandler - it means the field is not a handler and we skip it
-		} catch (IllegalArgumentException | SecurityException e) {
-			// failed to reflect, just don't set the route
+		} catch (IllegalArgumentException | SecurityException | IllegalAccessException e) {
+			log.error("Failed to configure handler for " + path + ": " + e,e);
 		}
 		return false;
-	}
-
-	private static boolean isBlocking(Field field) {
-		return Objects.nonNull(field.getAnnotation(Blocking.class));
-	}
-
-	private static boolean isFailHandler(Field field) {
-		return Objects.nonNull(field.getAnnotation(OnFail.class));
-	}
-
-	private static <T extends Annotation> String uriFromRoutingAnnotation(Field field, Class<T> anot) {
-		T spec = field.getAnnotation(anot);
-		if (spec == null) return null;
-		try {
-			// all routing annotations store the URI path in `value()`
-			return spec.getClass().getMethod("value").invoke(spec).toString();
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-			return null; // I don't know what it failed on, but it means it doesn't fit
-		} 
 	}
 
 	/**
