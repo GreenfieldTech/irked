83f6903a14599e800e5a0324ecab675bfecb6fbe
Oded Arbel <oded@geek.co.il>
2017-12-10 11:54:48 +0200

    add routes with @Consumes configuration

--- src/main/java/tech/greenfield/vertx/irked/annotations/ConsumesSpecs.java
+++ src/main/java/tech/greenfield/vertx/irked/annotations/ConsumesSpecs.java
@@ -1,13 +1,11 @@
 package tech.greenfield.vertx.irked.annotations;
 
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Repeatable(ConsumesSpecs.class)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Consumes {
+public @interface ConsumesSpecs {
 
-	String value();
-
+	Consumes[] value();
+	
 }
