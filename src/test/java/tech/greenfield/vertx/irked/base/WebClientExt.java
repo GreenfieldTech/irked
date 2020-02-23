b70ecb7827216f435a8bdc51601b52dcf68761ba
Oded Arbel <oded@geek.co.il>
2020-02-23 19:21:53 +0200

    more bringup for vert.x 4

---
+++ src/test/java/tech/greenfield/vertx/irked/base/WebClientExt.java
@@ -0,0 +1,61 @@
+package tech.greenfield.vertx.irked.base;
+
+import java.util.concurrent.CompletableFuture;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.*;
+import io.vertx.core.http.impl.headers.VertxHttpHeaders;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.client.impl.WebClientBase;
+
+public class WebClientExt extends WebClientBase {
+
+	private static HttpClient client;
+
+	public WebClientExt(Vertx vertx, WebClientOptions options) {
+		super(client = vertx.createHttpClient(), options);
+	}
+	
+	@Override
+	public HttpRequestExt<Buffer> get(int port, String host, String requestURI) {
+		return new HttpRequestExt<Buffer>(super.get(port, host, requestURI));
+	}
+
+	@Override
+	public HttpRequestExt<Buffer> post(int port, String host, String requestURI) {
+		return new HttpRequestExt<Buffer>(super.post(port, host, requestURI));
+	}
+	
+	@Override
+	public HttpRequestExt<Buffer> put(int port, String host, String requestURI) {
+		return new HttpRequestExt<Buffer>(super.put(port, host, requestURI));
+	}
+
+	@Override
+	public HttpRequestExt<Buffer> delete(int port, String host, String requestURI) {
+		return new HttpRequestExt<Buffer>(super.delete(port, host, requestURI));
+	}
+	
+	public CompletableFuture<WebSocket> websocket(int port, String host, String requestURI) {
+		CompletableFuture<WebSocket> fut = new CompletableFuture<>();
+		client.webSocket(port, host, requestURI, res -> {
+			if (res.failed()) fut.completeExceptionally(res.cause());
+			else fut.complete(res.result());
+		});
+		return fut;
+	}
+
+	public CompletableFuture<WebSocket> websocket(int port, String host, String requestURI, VertxHttpHeaders headers) {
+		CompletableFuture<WebSocket> fut = new CompletableFuture<>();
+		client.webSocket(new WebSocketConnectOptions()
+				.setPort(port)
+				.setHost(host)
+				.setURI(requestURI)
+				.setHeaders(headers), res -> {
+			if (res.failed()) fut.completeExceptionally(res.cause());
+			else fut.complete(res.result());
+		});
+		return fut;
+	}
+}
