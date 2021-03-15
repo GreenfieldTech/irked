83888411b6df3789fb1a1afb98ba25f6067ed461
Oded Arbel <oded@geek.co.il>
2021-03-15 13:48:11 +0200

    how did this succeed?

--- src/test/java/tech/greenfield/vertx/irked/TestBadController.java
+++ src/test/java/tech/greenfield/vertx/irked/TestBadController.java
@@ -1,11 +1,17 @@
 package tech.greenfield.vertx.irked;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.Test;
 
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxTestContext;
 import tech.greenfield.vertx.irked.annotations.Get;
 import tech.greenfield.vertx.irked.base.TestBase;
+import tech.greenfield.vertx.irked.exceptions.InvalidRouteConfiguration;
 
 public class TestBadController extends TestBase {
 
@@ -23,12 +29,22 @@ public class TestBadController extends TestBase {
 	
 	@Test
 	public void testInvalidFieldHandlerError(VertxTestContext context, Vertx vertx) {
-		deployController(new TestControllerBadField(), vertx, context.succeedingThenComplete());
+		deployController(new TestControllerBadField(), vertx, context.failing(t -> {
+			assertThat(t, is(instanceOf(InvalidRouteConfiguration.class)));
+			assertThat(t.getMessage(), containsString("invalidHandler"));
+			assertThat(t.getMessage(), containsString("not a valid handler or controller"));
+			context.completeNow();
+		}));
 	}
 	
 	@Test
 	public void testInvalidMethodHandlerError(VertxTestContext context, Vertx vertx) {
-		deployController(new TestControllerBadMethod(), vertx, context.succeedingThenComplete());
+		deployController(new TestControllerBadMethod(), vertx, context.failing(t -> {
+			assertThat(t, is(instanceOf(InvalidRouteConfiguration.class)));
+			assertThat(t.getMessage(), containsString("invalidMethod"));
+			assertThat(t.getMessage(), containsString("RoutingContext as first parameter"));
+			context.completeNow();
+		}));
 	}
 
 }
