8eabb3f5d099219927153a58de4b0bc37524f008
Oded Arbel <oded@geek.co.il>
2021-03-16 09:54:11 +0200

    cleanup test

--- src/test/java/tech/greenfield/vertx/irked/TestAuthDigest.java
+++ src/test/java/tech/greenfield/vertx/irked/TestAuthDigest.java
@@ -6,16 +6,16 @@ import static org.hamcrest.Matchers.is;
 import static tech.greenfield.vertx.irked.Matchers.*;
 
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxTestContext;
@@ -64,6 +64,7 @@ public class TestAuthDigest extends TestBase {
 		WebHandler failure = Request.failureHandler();
 	}
 
+	private static final Logger log = LoggerFactory.getLogger(TestAuthDigest.class);
 	private static final String userPass = "password";
 	private static final String userName = "user";
 	private Buffer postdata = Buffer.buffer("data");
@@ -77,61 +78,46 @@ public class TestAuthDigest extends TestBase {
 	public void testGetAuthed(VertxTestContext context, Vertx vertx) {
 		Checkpoint async = context.checkpoint();
 		getClient(vertx).get(port, "localhost", "/auth").sendP()
-		.thenCompose(res -> testGetAuthedAuth(vertx, res))
-		.exceptionally(failureHandler(context))
-		.thenRun(async::flag);
-	}
-
-	private CompletableFuture<Void> testGetAuthedAuth(Vertx vertx, HttpResponse<Buffer> res) {
-		assertThat(res, is(status(new Unauthorized())));
-		try {
+		.thenCompose(res -> {
+			assertThat(res, is(status(Unauthorized.class)));
 			Map<String, String> auth = parseAuthHeader(res.getHeader("WWW-Authenticate"));
 			DigestAuthorizationToken tok = new DigestAuthorizationToken(auth.get("realm"), "GET", "/auth", userName, userPass, auth.get("nonce"));
-			return getClient(vertx).get(port, "localhost", "/auth")
-			.putHeader("Authorization", tok.generateAuthrizationHeader())
-			.sendP()
-			.thenAccept(res2 -> testGetAuthedCheckResponse(res2));
-		} catch (Exception e) {
-			return CompletableFuture.failedFuture(e);
-		}
-	}
-
-	private void testGetAuthedCheckResponse(HttpResponse<Buffer> res) {
-		assertThat(res, isOK());
-		assertThat(res.body().toString(), is(equalTo("OK")));
+			return getClient(vertx).get(port, "localhost", "/auth").putHeader("Authorization", tok.generateAuthrizationHeader())
+					.sendP();
+		})
+		.thenAccept(res -> {
+			assertThat(res, isOK());
+			assertThat(res.body().toString(), is(equalTo("OK")));
+		})
+		.exceptionally(failureHandler(context))
+		.thenRun(async::flag);
 	}
 
 	@Test
 	public void testPostAuthed(VertxTestContext context, Vertx vertx) {
 		Checkpoint async = context.checkpoint();
-		getClient(vertx).post(port, "localhost", "/auth-int")
-		.putHeader("Content-Length", String.valueOf(postdata.length()))
-		.sendP(postdata)
-		.thenCompose(res -> testPostAuthedAuth(res, vertx))
+		getClient(vertx).post(port, "localhost", "/auth-int").putHeader("Content-Length", String.valueOf(postdata.length())).sendP(postdata)
+		.thenCompose(res -> {
+			assertThat(res, is(status(new Unauthorized())));
+			Map<String, String> auth = parseAuthHeader(res.getHeader("WWW-Authenticate"));
+			DigestAuthorizationToken tok = new DigestAuthorizationToken(auth.get("realm"), "POST", "/auth-int", postdata, userName, userPass,
+					auth.get("nonce"), DigestAuthenticate.generateNonce("", 300));
+			return getClient(vertx).post(port, "localhost", "/auth-int").putHeader("Authorization", tok.generateAuthrizationHeader())
+					.putHeader("Content-Length", String.valueOf(postdata.length())).sendP(postdata);
+		})
+		.thenAccept(res -> {
+			assertThat(res, isOK());
+			assertThat(res.body().toString(), is(equalTo("OK")));
+		})
 		.exceptionally(failureHandler(context))
 		.thenRun(async::flag);
 	}
 
-	private CompletableFuture<Void> testPostAuthedAuth(HttpResponse<Buffer> res, Vertx vertx) {
-		assertThat(res, is(status(new Unauthorized())));
-		try {
-			Map<String, String> auth = parseAuthHeader(res.getHeader("WWW-Authenticate"));
-			DigestAuthorizationToken tok = new DigestAuthorizationToken(auth.get("realm"), "POST", "/auth-int", postdata, userName, userPass, auth.get("nonce"), DigestAuthenticate.generateNonce("", 300));
-			return getClient(vertx).post(port, "localhost", "/auth-int")
-			.putHeader("Authorization", tok.generateAuthrizationHeader())
-			.putHeader("Content-Length", String.valueOf(postdata.length()))
-			.sendP(postdata)
-			.thenAccept(res2 -> testGetAuthedCheckResponse(res2));
-		} catch (Exception e) {
-			return CompletableFuture.failedFuture(e);
-		}
-	}
-
-	private Map<String,String> parseAuthHeader(String authHeader) throws Exception {
+	private Map<String,String> parseAuthHeader(String authHeader) {
 		String[] authparts = authHeader.split(" ",2);
-		if (!"Digest".equals(authparts[0]) || authparts.length != 2)
-			throw new Exception("Unexpected auth type: " + authparts[0]);
-		System.err.println("Authorization header: " + authHeader);
+		assertThat(authparts.length, is(equalTo(2)));
+		assertThat(authparts[0], is(equalTo("Digest")));
+		log.info("Authorization header: " + authHeader);
 		return StreamSupport.stream(ParameterEncodedAuthorizationToken.parseParameters(authparts[1]).spliterator(), false)
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 	}
