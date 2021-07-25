a8b432bbd0b038bd3b1485b9d3cb466ef93a6943
Oded Arbel <oded@geek.co.il>
2021-07-25 09:25:37 +0300

    fix example app use of async start
    show fluent API with no temporary variables

--- src/example/java/tech/greenfield/vertx/irked/example/App.java
+++ src/example/java/tech/greenfield/vertx/irked/example/App.java
@@ -4,7 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServerOptions;
 import tech.greenfield.vertx.irked.Controller;
-import tech.greenfield.vertx.irked.Router;
+import tech.greenfield.vertx.irked.Irked;
 import tech.greenfield.vertx.irked.annotations.Endpoint;
 import tech.greenfield.vertx.irked.helpers.Redirect;
 
@@ -12,17 +12,19 @@ public class App extends AbstractVerticle {
 
 	@Override
 	public void start(Promise<Void> startFuture) throws Exception {
-		Router router = new Router(vertx)
-				.configure(new APIv1(), "/1")
-				.configure(new APIv2(), "/2")
-				.configure(new Controller() {
-					@Endpoint("/")
-					WebHandler latest = r -> {
-						throw new Redirect("/2" + r.request().uri()).unchecked();
-					};
-				});
-		vertx.createHttpServer(new HttpServerOptions()).requestHandler(router)
-				.listen(config().getInteger("port", 8080));
+		vertx.createHttpServer(new HttpServerOptions())
+				.requestHandler(Irked.irked(vertx).router()
+						.configure(new ExampleAPIv1(), "/v1")
+						.configure(new ExampleAPIv2(), "/v2")
+						.configure(new Controller() {
+							@Endpoint("/")
+							WebHandler latest = r -> {
+								throw new Redirect("/v2" + r.request().uri()).unchecked();
+							};
+						}))
+				.listen(config().getInteger("port", 8080))
+				.onSuccess(v -> startFuture.complete())
+				.onFailure(t -> startFuture.fail(t));
 	}
 
 }
--- src/example/java/tech/greenfield/vertx/irked/example/ExampleAPIv2.java
+++ src/example/java/tech/greenfield/vertx/irked/example/ExampleAPIv2.java
@@ -4,7 +4,7 @@ import io.vertx.core.json.JsonObject;
 import tech.greenfield.vertx.irked.Controller;
 import tech.greenfield.vertx.irked.annotations.Endpoint;
 
-public class APIv2 extends Controller {
+public class ExampleAPIv2 extends Controller {
 
 	@Endpoint("/")
 	WebHandler start = r -> r.sendJSON(new JsonObject().put("version", 2)); 
