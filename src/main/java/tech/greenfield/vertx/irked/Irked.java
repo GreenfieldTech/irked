296ccfaa91a010020d228bfae1c53f39bb516385
Oded Arbel <oded@geek.co.il>
2021-07-25 09:17:23 +0300

    Add Vert.x stule factory method and javadocs

--- src/main/java/tech/greenfield/vertx/irked/Irked.java
+++ src/main/java/tech/greenfield/vertx/irked/Irked.java
@@ -2,17 +2,43 @@ package tech.greenfield.vertx.irked;
 
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import tech.greenfield.vertx.irked.exceptions.InvalidRouteConfiguration;
 
+/**
+ * Helper for creating Irked routers
+ * @author odeda
+ */
 public class Irked {
 
 	private Vertx vertx;
 
+	/**
+	 * Vert.x-styled Irked factory method.
+	 * @param vertx Vert.x instance in which to generated routers
+	 * @return a new Irked instance that can generate {@link Router}s and request handlers
+	 */
+	public static Irked irked(Vertx vertx) {
+		return new Irked(vertx);
+	}
+	
+	/**
+	 * Create new Irked instance that can generate request handlers
+	 * @deprecated please use {@link Irked#irked(Vertx)} instead
+	 * @param vertx Vert.x instance in which to generate routers
+	 */
 	public Irked(Vertx vertx) {
 		this.vertx = vertx;
 	}
 
+	/**
+	 * Create an HTTP request handler, that can be used in {@link HttpServer#requestHandler()},
+	 * with the provided set of APIs configured
+	 * @param apis set of Irked API controllers to configure routing for
+	 * @return an HTTP request handler for the Vert.x HTTP server
+	 * @throws InvalidRouteConfiguration in case one of the route configurations is invalid
+	 */
 	public Handler<HttpServerRequest> setupRequestHandler(Controller... apis) throws InvalidRouteConfiguration {
 		Router router = new Router(vertx);
 		for (Controller api : apis)
@@ -20,6 +46,19 @@ public class Irked {
 		return router;
 	}
 	
+	/**
+	 * Create a new Irked router
+	 * @return router that can be used to configure Irked API controllers and handle HTTP requests from Vert.x HTTP server
+	 */
+	public Router router() {
+		return new Router(vertx);
+	}
+	
+	/**
+	 * Create a new Irked router
+	 * @param vertx Vert.x instance in which to generate routers
+	 * @return router that can be used to configure Irked API controllers and handle HTTP requests from Vert.x HTTP server
+	 */
 	public static Router router(Vertx vertx) {
 		return new Router(vertx);
 	}
