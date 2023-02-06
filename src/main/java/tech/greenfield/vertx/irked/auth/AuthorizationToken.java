c57cc7da953d162f41b31048221674bd609eb721
Oded Arbel <oded@geek.co.il>
2023-02-06 11:45:13 +0200

    remove the service loader for authorization tokens due to security concerns.
    The service loader is a problem in the case of fuzzing attacks as it
    will hit the disk every time it is asked to load an unknown
    implementation and will not cache negative results (not that we expect
    it to waste memory on that). By moving to a static list of
    implementations we lose the ability for users to extend the
    implementation with non-standard authorization tokens - but hopefully
    this is not a requested feature.

--- src/main/java/tech/greenfield/vertx/irked/auth/AuthorizationToken.java
+++ src/main/java/tech/greenfield/vertx/irked/auth/AuthorizationToken.java
@@ -1,12 +1,60 @@
 package tech.greenfield.vertx.irked.auth;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 import java.util.Objects;
-import java.util.ServiceLoader;
 
 public class AuthorizationToken {
 	
 	private String token;
 	private String type;
+	
+	public enum TokenTypes {
+		// not actually a type - just a base implementation:
+		// PARAMETER(ParameterEncodedAuthorizationToken.class)
+		
+		NULL(NullAuthorizationToken.class),
+		SIMPLE(SimpleAuthrizationToken.class),
+		BASIC(BasicAuthorizationToken.class),
+		BEARER(BearerAuthorizationToken.class),
+		DIGEST(DigestAuthorizationToken.class),
+		HOB(HOBAAuthorizationToken.class),
+		MUTUAL(MutualAuthorizationToken.class),
+		NEGOTIATE(NegotiateAuthorizationToken.class),
+		OAUTH(OAuthAuthorizationToken.class);
+
+		private Class<? extends AuthorizationToken> clz;
+
+		TokenTypes(Class<? extends AuthorizationToken> clz) {
+			this.clz = clz;
+		}
+
+		public static Iterable<AuthorizationToken> instances() {
+			return new Iterable<AuthorizationToken>() {
+				private TokenTypes[] types = values();
+				@Override
+				public Iterator<AuthorizationToken> iterator() {
+					return new Iterator<AuthorizationToken>() {
+						private int index = 0;
+						private int max = types.length;
+						@Override
+						public boolean hasNext() {
+							return index < max;
+						}
+						@Override
+						public AuthorizationToken next() {
+							try {
+								return types[index++].clz.getConstructor().newInstance();
+							} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+									| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+								System.err.print("Unexpected exception instantiating a known authorization token type! this shouldn't happen");
+								throw new Error(e);
+							}
+						}
+					};
+				}};
+		}
+	}
 
 	protected AuthorizationToken() {
 	}
@@ -17,7 +65,7 @@ public class AuthorizationToken {
 		String[] parts = authorizationHeader.split("\\s+",2);
 		if (parts.length == 1)
 			return new SimpleAuthrizationToken(parts[0]);
-		for (AuthorizationToken a : ServiceLoader.load(AuthorizationToken.class)) {
+		for (AuthorizationToken a : TokenTypes.instances()) {
 			if (a.supports(parts[0]))
 				return a.update(parts[0], parts[1]);
 		}
