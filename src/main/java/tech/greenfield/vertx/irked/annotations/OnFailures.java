0e870b8178ab378a0312f2980dc6e99d6bbdf941
Oded Arbel <oded@geek.co.il>
2023-03-02 14:29:56 +0200

    Allow multiple @OnFail specifications for the same handler
    This is useful to have one handler that handled multiple exceptions or
    status codes

---
+++ src/main/java/tech/greenfield/vertx/irked/annotations/OnFailures.java
@@ -0,0 +1,15 @@
+package tech.greenfield.vertx.irked.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Repeatable container for <code>@OnFail</code>
+ * @author odeda
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OnFailures {
+
+	OnFail[] value();
+	
+}
