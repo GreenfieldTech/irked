0942584172897e0b505d92e0d21baea036365c70
Oded Arbel <oded@geek.co.il>
2024-08-22 20:39:10 +0300

    add support for request context re-programming on the fly, for methods

--- src/main/java/tech/greenfield/vertx/irked/RouteConfigurationMethod.java
+++ src/main/java/tech/greenfield/vertx/irked/RouteConfigurationMethod.java
@@ -35,6 +35,7 @@ public class RouteConfigurationMethod extends RouteConfiguration {
 	private final Method method;
 	private final Parameter[] params;
 	private final Map<String, Function<Request, Object>> paramResolvers = new HashMap<>();
+	private Function<Request,Request> routingContextResolver;
 
 	public RouteConfigurationMethod(Controller impl, Router router, Method m) throws InvalidRouteConfiguration {
 		super(impl, router, m.getAnnotations());
@@ -54,6 +55,10 @@ public class RouteConfigurationMethod extends RouteConfiguration {
 		if (paramErrors.isPresent())
 			throw new InvalidRouteConfiguration(String.format("Method %1$s.%2$s contains parameters that cannot be resolved: %3$s",
 					m.getDeclaringClass().getName(), m.getName(), paramErrors.get()));
+		// check if we require non-trivial routing context construction and have a local provider available for it
+		var requestType = params[0].getType();
+		if (!requestType.isAssignableFrom(Request.class)) // we know that the reverse is true, so requestType must be a Request sub-type
+			routingContextResolver = findRoutingContextResolver(impl, requestType);
 	}
 
 	private Set<String> parseRouteParams(String[] possibleURIs) {
@@ -173,16 +178,10 @@ public class RouteConfigurationMethod extends RouteConfiguration {
 		return new Handler<Request>() {
 			@Override
 			public void handle(Request r) {
-				// run time check for correct type
-				// we support working with methods that take specializations for Request, we'll rely on the specific implementation's
-				// getRequest() to provide the correct type
-				if (!params[0].getType().isAssignableFrom(r.getClass())) {
-					r.fail(new InternalServerError("Invalid request handler " + this + " - can't handle request of type " + r.getClass()));
-					return;
-				}
-				
 				try {
-					method.invoke(impl, createParamBlock(r));
+					method.invoke(impl, createParamBlock(resolveRequestContext(r, params[0].getType())));
+				} catch (RoutingContextImplException e) {
+					r.fail(new InternalServerError(e.getMessage(), e));
 				} catch (InvocationTargetException e) { // user exception
 					handleUserException(r, e.getCause(), "method " + method);
 				} catch (IllegalAccessException e) { // shouldn't happen because we setAccessible above
@@ -239,6 +238,76 @@ public class RouteConfigurationMethod extends RouteConfiguration {
 		for (int i = 1; i < params.length; i++)
 			invokeParams[i] = paramResolvers.computeIfAbsent(params[i].getName(), (req -> null)).apply(r);
 		return invokeParams;
+	}
+
+	public class RoutingContextImplException extends RuntimeException {
+		private static final long serialVersionUID = 3549348052777343128L;
+		public RoutingContextImplException(String message) { super(message); }
+		public RoutingContextImplException(Exception e) {
+			super(String.format("Failed to construct routing context param for %s from Request instance", RouteConfigurationMethod.this), e);
+		}
+	}
+	
+	/**
+	 * Try to resolve the handler requested routing context type by instantiating the requested type if needed.
+	 * @param r current routing context instance. This can be in itself a Request sub-type, which the handler's preferred
+	 * context type might require for initialization
+	 * @param programableContext The routing context type required by the handler
+	 * @return the current routing context type if it is already sufficient, or a new instance of the required type if
+	 * it can be successfully trivially constructed.
+	 * @throws RoutingContextImplException if the requested type is not a Request sub-type or it cannot be successfully
+	 * constructed.
+	 */
+	private Request resolveRequestContext(Request r, Class<?> programableContext) throws RoutingContextImplException {
+		if (routingContextResolver != null)
+			r = routingContextResolver.apply(r);
+		if (programableContext.isAssignableFrom(r.getClass()))
+			return r; // Controller implemented getRequest() correctly, or another supplier, no more work for us
+		// try to instantiate the required type if it is a specialization of Request and contains a trivial c'tor
+		if (!Request.class.isAssignableFrom(programableContext))
+			throw new RoutingContextImplException(String.format(
+					"Invalid request handler %s: routing context param %s is not a Request sub-type!",
+					this, programableContext));
+		for (var ctor : programableContext.getConstructors()) {
+			if (ctor.getParameterCount() != 1)
+				continue;
+			var p0 = ctor.getParameterTypes()[0];
+			if (p0.isAssignableFrom(r.getClass()))
+				try {
+					return (Request) ctor.newInstance(r);
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					throw new RoutingContextImplException(e);
+				}
+		}
+		throw new RoutingContextImplException(String.format(
+				"Invalid request handler %s: routing context param %s is not trivially constructed from a Request instance!"
+						+ " If you want to use non-trivially constructed programmable requests contexts, implement Controller.getRequest(Request)",
+				this, programableContext));
+
+	}
+	
+	/**
+	 * Check if we can statically locate an appropriate routing context supplier in the controller implementation.
+	 * @param impl The controller implementation which will be used to call the supplier
+	 * @param requestType the Request sub-type that we need to resolve
+	 * @return a trivial supplier, or null if none found
+	 */
+	private Function<Request, Request> findRoutingContextResolver(Controller impl, Class<?> requestType) {
+		// see if there's a provider for this type in the Controller implementation
+		for (Class<?> ctrImpl = impl.getClass(); ctrImpl != Controller.class; ctrImpl = ctrImpl.getSuperclass()) {
+			for (var m : ctrImpl.getDeclaredMethods()) {
+				if (m.getReturnType().equals(requestType) && m.getParameterCount() == 1 && m.getParameterTypes()[0].isAssignableFrom(Request.class))
+					return r -> {
+						m.setAccessible(true);
+						try {
+							return (Request) m.invoke(impl, r);
+						} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+							throw new RoutingContextImplException(e);
+						}
+					};
+			}
+		}
+		return null;
 	}
 
 }
---
+++ src/test/java/tech/greenfield/vertx/irked/TestImplicitRequestContext.java
@@ -0,0 +1,198 @@
+package tech.greenfield.vertx.irked;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static tech.greenfield.vertx.irked.Matchers.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxTestContext;
+import tech.greenfield.vertx.irked.annotations.Endpoint;
+import tech.greenfield.vertx.irked.annotations.Get;
+import tech.greenfield.vertx.irked.annotations.OnFail;
+import tech.greenfield.vertx.irked.base.TestBase;
+import tech.greenfield.vertx.irked.status.InternalServerError;
+
+public class TestImplicitRequestContext extends TestBase {
+
+	public static class IdContext extends Request {
+		private String id;
+
+		public IdContext(Request req) {
+			super(req);
+			id = req.pathParam("id");
+		}
+		
+		public String getId() {
+			return id;
+		}
+
+	}
+	
+	public static class ExtendedIdContext extends IdContext {
+		private String foo;
+		
+		public ExtendedIdContext(Request req) {
+			super(req);
+			foo = req.pathParam("foo");
+		}
+		
+		public String getFoo() {
+			return foo;
+		}
+	}
+	
+	public class NonTrivialContext extends ExtendedIdContext {
+
+		private String preset;
+
+		public NonTrivialContext(Request req, String preset) {
+			super(req);
+			this.preset = preset;
+		}
+		
+		public String getPreset() {
+			return preset;
+		}
+	}
+	
+	public static class BadContext extends Request {
+		public BadContext(int outerContext) {
+			super(null);
+		}
+		
+	}
+	
+	public static class ThrowingContext extends Request {
+
+		public ThrowingContext(RoutingContext outerContext) {
+			super(outerContext);
+			throw new IllegalStateException();
+		}
+		
+	}
+	
+	public class TestController extends Controller {
+		
+		@Get("/good/:id")
+		public void retrieve(IdContext r) {
+			r.sendJSON(new JsonObject().put("id", r.getId()));
+		}
+
+		@Get("/good/:id/:foo")
+		public void checkFoo(ExtendedIdContext r) {
+			r.sendJSON(new JsonObject().put("id", r.getId()).put("foo", r.getFoo()));
+		};
+		
+		@Get("/good-with-preset/:id/:foo")
+		public void retrievePreset(NonTrivialContext r) {
+			r.sendJSON(new JsonObject().put("id", r.getId()).put("foo", r.getFoo())
+					.put("preset", r.getPreset()));
+		}
+
+		@Get("/bad/not-constructible")
+		public void badHandler(BadContext ctx) {
+			System.err.println("This bad handler will never be called");
+		}
+		
+		@Get("/bad/throwing-ctor")
+		public void anotherBadHandler(ThrowingContext ctx) {
+			System.err.println("This another bad handler will never be called");
+		}
+		
+		@OnFail
+		@Endpoint
+		WebHandler failHandler = Request.failureHandler();
+		
+		@SuppressWarnings("unused")
+		private NonTrivialContext createNonTrivialContext(Request r) {
+			return new NonTrivialContext(r, "magic");
+		}
+	}
+
+	@BeforeEach
+	public void deployServer(VertxTestContext context, Vertx vertx) {
+		deployController(new TestController(), vertx, context.succeedingThenComplete());
+	}
+
+	@Test
+	public void testPassIdToMethod(VertxTestContext context, Vertx vertx) {
+		log.info("Testing get ID");
+		String id = "item-name";
+		getClient(vertx).get(port, "localhost", "/good/" + id).send().map(res -> {
+			assertThat(res, isSuccess());
+			JsonObject o = res.bodyAsJsonObject();
+			assertThat(o.getString("id"), equalTo(id));
+			return null;
+		})
+		.onComplete(context.succeedingThenComplete());
+	}
+
+	@Test
+	public void testPassFooToMethod(VertxTestContext context, Vertx vertx) {
+		log.info("Testing get extended ID");
+		String id = "item-name"; String foo = "bar";
+		getClient(vertx).get(port, "localhost", "/good/" + id + "/" + foo).send().map(res -> {
+			assertThat(res, isSuccess());
+			JsonObject o = res.bodyAsJsonObject();
+			assertThat(o.getString("id"), is(equalTo(id)));
+			assertThat(o.getString("foo"), is(equalTo(foo)));
+			return null;
+		})
+		.onComplete(context.succeedingThenComplete());
+	}
+	
+	@Test
+	public void testPassWithPreset(VertxTestContext ctx, Vertx vertx) {
+		log.info("Testing get with preset");
+		String id = "item-name"; String foo = "baz";
+		getClient(vertx).get(port, "localhost", "/good-with-preset/" + id + "/" + foo).send().map(res -> {
+			assertThat(res, isSuccess());
+			JsonObject o = res.bodyAsJsonObject();
+			assertThat(o.getString("id"), is(equalTo(id)));
+			assertThat(o.getString("foo"), is(equalTo(foo)));
+			assertThat(o.getString("preset"), is(equalTo("magic")));
+			return null;
+		})
+		.onComplete(ctx.succeedingThenComplete());
+
+	}
+	
+	@Test
+	@Timeout(timeUnit = TimeUnit.MINUTES, value = 5)
+	public void testBadNoConstructible(VertxTestContext context, Vertx vertx) {
+		log.info("Testing invalid ctor");
+		getClient(vertx).get(port, "localhost", "/bad/not-constructible")
+		.send().map(res -> {
+			assertThat(res, status(InternalServerError.class));
+			var err = res.bodyAsJsonObject();
+			assertThat(err.getBoolean("status"), is(false));
+			assertThat(err.getString("message"), is(equalTo("Invalid request handler class tech.greenfield.vertx.irked.TestImplicitRequestContext$TestController::badHandler: routing context param class tech.greenfield.vertx.irked.TestImplicitRequestContext$BadContext is not trivially constructed from a Request instance! If you want to use non-trivially constructed programmable requests contexts, implement Controller.getRequest(Request)")));
+			return null;
+		})
+		.onComplete(context.succeedingThenComplete());
+	}
+
+	@Test
+	public void testBadThrowingCtor(VertxTestContext context, Vertx vertx) {
+		log.info("Testing throwing ctor");
+		getClient(vertx).get(port, "localhost", "/bad/throwing-ctor")
+		.send().map(res -> {
+			assertThat(res, status(InternalServerError.class));
+			var err = res.bodyAsJsonObject();
+			assertThat(err.getBoolean("status"), is(false));
+			assertThat(err.getString("message"), is(equalTo("Failed to construct routing context param for class tech.greenfield.vertx.irked.TestImplicitRequestContext$TestController::anotherBadHandler from Request instance")));
+			return null;
+		})
+		.onComplete(context.succeedingThenComplete());
+	}
+
+}
