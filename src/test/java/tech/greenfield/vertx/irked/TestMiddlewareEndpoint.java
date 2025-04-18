f029ff3a56f16b71ec2e14696bea63cc473d0626
Oded Arbel <oded@geek.co.il>
2025-04-19 02:47:39 +0300

    add a unit test that shows the problem with the middleware not being call when mounted on a path with params

---
+++ src/test/java/tech/greenfield/vertx/irked/TestMiddlewareEndpoint.java
@@ -0,0 +1,54 @@
+package tech.greenfield.vertx.irked;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static tech.greenfield.vertx.irked.Matchers.isSuccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxTestContext;
+import tech.greenfield.vertx.irked.annotations.Endpoint;
+import tech.greenfield.vertx.irked.annotations.Get;
+import tech.greenfield.vertx.irked.base.TestBase;
+import tech.greenfield.vertx.irked.status.InternalServerError;
+import tech.greenfield.vertx.irked.status.OK;
+
+public class TestMiddlewareEndpoint extends TestBase {
+
+	public class TestController extends Controller {
+		@Endpoint
+		WebHandler middleware = r -> {
+			r.put("middleware-ran", true);
+			r.next();
+		};
+		
+		@Get("/")
+		WebHandler index = r -> {
+			if (r.<Boolean>get("middleware-ran") == Boolean.TRUE)
+				r.send(new OK());
+			else
+				r.send(new InternalServerError("Middleware was not called!"));
+		};
+	}
+	
+	public class TestMainController extends Controller {
+		@Endpoint("/test/:param")
+		TestController subController = new TestController();
+	}
+	
+	@BeforeEach
+	public void deployServer(VertxTestContext context, Vertx vertx) {
+		deployController(new TestMainController(), vertx, context.succeedingThenComplete());
+	}
+	
+	@Test
+	public void testMiddlewareEndpointIsCalled(VertxTestContext context, Vertx vertx) {
+		getClient(vertx).get(port, "localhost", "/test/foo").send().map(res -> {
+			assertThat(res, isSuccess());
+			return null;
+		})
+		.onComplete(context.succeedingThenComplete());
+	}
+	
+}
